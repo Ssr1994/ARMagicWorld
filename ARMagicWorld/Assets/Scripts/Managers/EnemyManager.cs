0defd99a14b1cba097ea1f17e206e533b0bd9d1c
Ssr1994 <illegalsimon@gmail.com>
2016-02-22 14:26:39 -0800

    added VirtualButtons

--- ARMagicWorld/Assets/Scripts/Managers/EnemyManager.cs
+++ ARMagicWorld/Assets/Scripts/Managers/EnemyManager.cs
@@ -26,6 +26,7 @@ public class EnemyManager : MonoBehaviour
         if(playerHealth.currentHealth <= 0f)
             return;
 
+		// Or use enemyNum to balance game
 		if (++numSpawned >= levelPoint && spawnTime > spawnTimeReduce) {
 			numSpawned = 0;
 			spawnTime -= spawnTimeReduce;
@@ -36,7 +37,7 @@ public class EnemyManager : MonoBehaviour
         int spawnPointIndex = Random.Range (0, spawnPoints.Length);
 
         GameObject spawned = Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation) as GameObject;
-		spawned.transform.parent = imageTarget.transform;
+		//spawned.transform.parent = imageTarget.transform;
 		imageTarget.GetComponent<VirtualButtonManager> ().AssignVirtualButton (spawned);
 		enemyNum++;
     }
